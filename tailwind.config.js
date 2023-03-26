e0db349d2b0a4de126ce43b898a481935546f7e4
iAmMuadh <adeyemimuaz1@gmail.com>
2023-03-26 01:18:03 +0100

    styles completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      white : 'hsl(0, 0%, 100%)',
+      LightGrey: 'hsl(217, 12%, 63%)',
+      mediumGrey: 'hsl(216, 12%, 54%)',
+      darkBlue: 'hsl(213, 19%, 18%)',
+      veryDarkBlue: 'hsl(216, 12%, 8%)',
+      orange: 'hsl(25, 97%, 53%)',
+      additional: '#374151'
+    },
   },
   plugins: [],
 }
